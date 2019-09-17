dcc8db1b8e28ae24206c184524176f4e561eb3f1
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-17 04:57:51 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,7 +4,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [{name: "Benvolio", age: "17", status: "worried"},{name: "Mercutio", age: "19", status: "hot=headed"}
+      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"},{name: "Mercutio", age: "19", attitude: "hot=headed"}
       ]
    }, 
    :capulet => {
@@ -12,7 +12,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
